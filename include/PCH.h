5c2d23b264f6fdb668b53f056399343a2bcde668
SkyHorizon3 <neuner.r@gmx.de>
2026-01-09 14:19:54 +0100

    feat: 2026 update

--- include/PCH.h
+++ include/PCH.h
@@ -3,7 +3,18 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
+#include "Plugin.h"
+
 using namespace std::literals;
 
-#include <spdlog/sinks/basic_file_sink.h>
-#include "Plugin.h"
+namespace stl
+{
+	using namespace SKSE::stl;
+
+	template <class T>
+	void write_thunk_call(std::uintptr_t a_src)
+	{
+		auto& trampoline = SKSE::GetTrampoline();
+		T::func = trampoline.write_call<5>(a_src, T::thunk);
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,61 @@
-﻿#include "Hooks.h"
-
-void SetupLog()
+﻿
+struct CombatAIHook
 {
-	auto logsFolder = SKSE::log::log_directory();
-	if (!logsFolder)
+	static bool thunk(RE::Actor* a_target)
 	{
-		SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
-	}
+		auto result = func(a_target);
+		if (!result) {
+			const auto ui = RE::UI::GetSingleton();
+			if (ui && (ui->IsMenuOpen(RE::LoadingMenu::MENU_NAME) || ui->IsMenuOpen(RE::FaderMenu::MENU_NAME))) {
+				return true;
+			}
+		}
 
-	auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
-	auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
-	auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
-	auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+		return result;
+	};
+	static inline REL::Relocation<decltype(thunk)> func;
 
-#ifndef NDEBUG
-	loggerPtr->set_level(spdlog::level::trace);
-	loggerPtr->flush_on(spdlog::level::trace);
-#else
-	loggerPtr->set_level(spdlog::level::info);
-	loggerPtr->flush_on(spdlog::level::info);
-#endif
-
-	spdlog::set_default_logger(std::move(loggerPtr));
-}
-
-void MessageListener(SKSE::MessagingInterface::Message* message)
-{
-	switch (message->type)
+	static void Install()
 	{
-	case SKSE::MessagingInterface::kPostLoad:
-	{
-		LoadingScreenTruce::CombatAIHook::InstallHook();
+		REL::Relocation<std::uintptr_t> callTarget{ RELOCATION_ID(32469, 33216), REL::Relocate(0xFF, 0xEC) };
+		stl::write_thunk_call<CombatAIHook>(callTarget.address());
 	}
-	break;
-
-	default:
-		break;
-
-	}
-}
-
-SKSEPluginLoad(const SKSE::LoadInterface* skse)
-{
-	SKSE::Init(skse);
-	SetupLog();
-
-	SKSE::GetMessagingInterface()->RegisterListener(MessageListener);
-
-	SKSE::log::info("{} v{} loaded", Plugin::NAME, Plugin::VERSION);
-
-	return true;
-}
+};
 
 #define DLLEXPORT __declspec(dllexport)
 
-extern "C" DLLEXPORT const auto SKSEPlugin_Version = []() noexcept {
-	SKSE::PluginVersionData v;
-	v.PluginName(Plugin::NAME.data());
-	v.PluginVersion(Plugin::VERSION);
-	v.UsesAddressLibrary(true);
-	v.HasNoStructUse();
-	return v;
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []()
+	{
+		SKSE::PluginVersionData v;
+		v.PluginName(Plugin::NAME);
+		v.AuthorName("Maxsu SkyHorizon"sv);
+		v.PluginVersion(Plugin::VERSION);
+		v.UsesAddressLibrary();
+		v.UsesNoStructs();
+		return v;
 	}
 ();
 
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo * pluginInfo)
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface*, SKSE::PluginInfo* pluginInfo)
 {
 	pluginInfo->name = SKSEPlugin_Version.pluginName;
 	pluginInfo->infoVersion = SKSE::PluginInfo::kVersion;
 	pluginInfo->version = SKSEPlugin_Version.pluginVersion;
+	return true;
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse)
+{
+	SKSE::Init(skse, true);
+
+	spdlog::set_pattern("[%H:%M:%S:%e] [%l] %v"s);
+	spdlog::set_level(spdlog::level::info);
+	spdlog::flush_on(spdlog::level::info);
+
+	SKSE::log::info("Game version: {}", skse->RuntimeVersion());
+
+	SKSE::AllocTrampoline(14);
+	CombatAIHook::Install();
+
 	return true;
 }
